f21a1b5f7001fa6fdbf8ba922f117a24010929a6
O. Marchenko <alex.mar4enko@gmail.com>
2022-03-17 11:44:06 -0700

    feat: add url validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,59 @@
-import { camelCase } from 'lodash';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// eslint-disable-next-line no-console
-console.log(camelCase('Hello World!'));
+import { string } from 'yup';
+import onChange from 'on-change';
+
+const formElement = document.querySelector('#rss-form');
+const feedbackElement = document.querySelector('.feedback');
+
+const watchedState = onChange({
+  feedList: [],
+  rssForm: {
+    url: null,
+    isValid: true,
+    errors: [],
+  },
+}, (path, value) => {
+  switch (path) {
+    case 'rssForm.isValid':
+      if (!value) {
+        formElement.elements.url.classList.add('is-invalid');
+      }
+      if (value) {
+        formElement.elements.url.classList.remove('is-invalid');
+        feedbackElement.textContent = '';
+        formElement.elements.url.focus();
+        formElement.elements.url.value = '';
+      }
+      break;
+    case 'rssForm.Errors':
+      feedbackElement.textContent = value.join(' ');
+      break;
+    default:
+      break;
+  }
+});
+
+const validateURL = (url) => {
+  const urlSchema = string()
+    .required()
+    .url('Must be valid url')
+    .notOneOf(watchedState.feedList, 'This RSS has been already added');
+  urlSchema.validate(url)
+    .then(() => {
+      watchedState.rssForm.isValid = true;
+      watchedState.rssForm.errors = [];
+      watchedState.feedList.push(url);
+    })
+    .catch((e) => {
+      watchedState.rssForm.isValid = false;
+      watchedState.rssForm.errors = e.errors;
+      throw new Error(e);
+    });
+};
+
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const url = formData.get('url');
+  validateURL(url);
+});
