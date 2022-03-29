c8234363996c1c2587521ca97be5002f26994520
O. Marchenko <alex.mar4enko@gmail.com>
2022-03-29 09:14:11 -0700

    test: fix ESM node loading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import run from './init';
 
-try {
-  run();
-} catch (error) {
-  console.log(error);
-}
+run();
